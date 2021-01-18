9e65e508c399dfa049d5760891fd007722ce9237
Ansh Kumar Gupta <63894658+Ansh7899@users.noreply.github.com>
2021-01-18 11:17:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from twilio.rest import Client
 #from twilio_credentials import cellphone, twilio_account, twilio_token, twilio_number
 
 msg = "Good Morning Love! Hope You Have An Amazing Day <3"
-cellphone = "+917840038110"
-twilio_number = "+16106001093"
-twilio_account = "AC16df31d7cf349814862ec1a2ea8f5287"
-twilio_token = "d0bb8168932c47a20ee732bcf090aae1"
+cellphone = "+91XXXXXXXXXXXXXXX"
+twilio_number = "+16XXXXXXXXXXXXXXXXX"
+twilio_account = "AXXXXXXXXXXXXXXXXXXXXXXXXX"
+twilio_token = "dXXXXXXXXXXXXXXXXXXXXXXXX"
 
 def send_message():
     client = Client(twilio_account, twilio_token)
